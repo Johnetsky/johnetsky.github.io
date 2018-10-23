868718cb35e2699056cdd6b1acd488645f7173fd
Johnetsky <44407118+Johnetsky@users.noreply.github.com>
2018-10-23 19:40:30 +0300

    Update program.js

--- program.js
+++ program.js
@@ -1,5 +1,5 @@
 function modifica()
 {
-  document.getElementById("id_Hello world").innerHTML="Hello";
+  document.getElementById("id_salut").innerHTML="Hello";
   document.getElementById("id_img").src="cat.jpg";
 }
