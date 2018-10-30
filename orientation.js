90fa5b0457e43e3f472506356203b51c07898ab7
Johnetsky <44407118+Johnetsky@users.noreply.github.com>
2018-10-30 19:45:10 +0200

    Update orientation.js

--- orientation.js
+++ orientation.js
@@ -1,4 +1,4 @@
-document.getElementById("id_logic_version").innerHTML = "Logic version: 2018.10.30.1";
+document.getElementById("id_logic_version").innerHTML = "Logic version: 2018.10.30.2";
 
 window.addEventListener("deviceorientation", la_schimbare_gyro);
 
