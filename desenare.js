23a5e80fe82f7bc5128527a98d2e7eafba653d31
Johnetsky <44407118+Johnetsky@users.noreply.github.com>
2018-11-20 18:46:35 +0200

    New update

--- desenare.js
+++ desenare.js
@@ -1,7 +1,9 @@
-document.getElementById("id_logic_version").innerHTML = "Logic version = 2018.11.20.2";
+document.getElementById("id_logic_version").innerHTML = "Logic version = 2018.11.20.3";
 
 var canvas = document.getElementById("id_canvas");
 canvas.addEventListener("touchstart", on_touch);
+canvas.addEventListener("touchmove", on_touch_move);
+
 var rect = canvas.getBoundingClientRect();
 
 function on_touch(e)
@@ -13,4 +15,15 @@ function on_touch(e)
 		context.arc(e.changedTouches.item(i).pageX - rect.left, e.changedTouches.item(i).pageY - rect.top, 20, 0, 2*Math.PI);
 		context.stroke();
 	}
+}
+
+function on_touch_move()
+{
+	for(var i=0; i<e.changedTouches.length; i++)
+	{
+		var context = canvas.getContext("2d");
+		context.beginPath();
+		context.arc(e.changedTouches.item(i).pageX - rect.left, e.changedTouches.item(i).pageY - rect.top, 20, 0, 2*Math.PI);
+		context.stroke();
+	}	
 }
