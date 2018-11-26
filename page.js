52bfda8ae26ef422b74820d6098687993e41e2dc
Johnetsky <44407118+Johnetsky@users.noreply.github.com>
2018-11-26 21:11:24 +0200

    Program for my personal page

---
+++ page.js
@@ -0,0 +1,17 @@
+var pisica = false;
+
+function modifica()
+{
+  document.getElementById("id_salut").innerHTML="Hello";
+  
+  if (pisica == false)
+  {
+    document.getElementById("id_img").src="caleferata.jpg";
+    pisica = true;
+  }
+  else
+  {
+    document.getElementById("id_img").src="cat.jpg";
+    pisica = false; 
+  }
+}
