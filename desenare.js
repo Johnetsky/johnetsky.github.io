1ec2f1e460dacb40228f86667444f91a8c72b967
Johnetsky <44407118+Johnetsky@users.noreply.github.com>
2018-11-27 18:42:23 +0200

    Add files via upload

--- desenare.js
+++ desenare.js
@@ -1,4 +1,4 @@
-document.getElementById("id_logic_version").innerHTML = "Logic version = 2018.11.27.3";
+document.getElementById("id_logic_version").innerHTML = "Logic version = 2018.11.27.4";
 
 var canvas = document.getElementById("id_canvas");
 canvas.addEventListener("touchstart", on_touch);
@@ -6,7 +6,6 @@ canvas.addEventListener("touchmove", on_touch_move);
 var rect = canvas.getBoundingClientRect();
 //var lastX =0;
 //var lastY =0;
-var last_position = {x: 0, y: 0, id: 0};
 var last_position_array = [];
 
 function on_touch(e)
@@ -19,6 +18,7 @@ function on_touch(e)
 		context.lineWidth = 1;
 		context.arc(e.changedTouches.item(i).pageX - rect.left, e.changedTouches.item(i).pageY - rect.top, 10, 0, 2*Math.PI);
 		context.stroke();
+		var last_position = {x: 0, y: 0, id: 0};
 		last_position.x = e.changedTouches.item(i).pageX;
 		last_position.y = e.changedTouches.item(i).pageY;
 		last_position.id = e.changedTouches.item(i).identifier;
